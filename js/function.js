a20e15195dc22b6fe9c172f20b9bd23aa1d4f09c
guaitaj <guaitaj@hotmail.com>
2018-02-14 21:43:42 -0400

    ACTILICE LOS ENLACES

--- js/function.js
+++ js/function.js
@@ -28,3 +28,66 @@ function openNav4() {
 function closeNav4() {
     document.getElementById("myNav4").style.width = "0%";
 }
+function openNav5() {
+    document.getElementById("myNav5").style.width = "100%";
+}
+
+function closeNav5() {
+    document.getElementById("myNav5").style.width = "0%";
+}
+function openNav6() {
+    document.getElementById("myNav6").style.width = "100%";
+}
+
+function closeNav6() {
+    document.getElementById("myNav6").style.width = "0%";
+}
+function openNav7() {
+    document.getElementById("myNav7").style.width = "100%";
+}
+
+function closeNav7() {
+    document.getElementById("myNav7").style.width = "0%";
+}
+function openNav8() {
+    document.getElementById("myNav8").style.width = "100%";
+}
+
+function closeNav8() {
+    document.getElementById("myNav8").style.width = "0%";
+}
+function openNav9() {
+    document.getElementById("myNav9").style.width = "100%";
+}
+
+function closeNav9() {
+    document.getElementById("myNav9").style.width = "0%";
+}
+function openNav10() {
+    document.getElementById("myNav10").style.width = "100%";
+}
+
+function closeNav10() {
+    document.getElementById("myNav10").style.width = "0%";
+}
+function openNav11() {
+    document.getElementById("myNav11").style.width = "100%";
+}
+
+function closeNav11() {
+    document.getElementById("myNav11").style.width = "0%";
+}
+function openNav12() {
+    document.getElementById("myNav12").style.width = "100%";
+}
+
+function closeNav12() {
+    document.getElementById("myNav12").style.width = "0%";
+}
+function openNav13() {
+    document.getElementById("myNav13").style.width = "100%";
+}
+
+function closeNav13() {
+    document.getElementById("myNav13").style.width = "0%";
+}
