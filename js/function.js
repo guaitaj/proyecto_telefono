2300f51482cfd1bb80a89a546d2f442a045dc15f
guaitaj <guaitaj@hotmail.com>
2018-02-14 21:10:37 -0400

    poniendo las imagenes

---
+++ js/function.js
@@ -0,0 +1,29 @@
+
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
+
+
+function openNav2() {
+    document.getElementById("myNav2").style.width = "100%";
+}
+
+function closeNav2() {
+    document.getElementById("myNav2").style.width = "0%";
+}
+
+function openNav3() {
+    document.getElementById("myNav3").style.width = "100%";
+}
+
+function closeNav3() {
+    document.getElementById("myNav3").style.width = "0%";
+}
+
+
+
+
