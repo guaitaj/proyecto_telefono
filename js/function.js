e3ac59a1fda49131b4a9afe486ecc26e2292f611
guaitaj <guaitaj@hotmail.com>
2018-02-14 21:32:13 -0400

    ACTILIZADO

--- js/function.js
+++ js/function.js
@@ -5,9 +5,7 @@ function openNav() {
 
 function closeNav() {
     document.getElementById("myNav").style.width = "0%";
-<<<<<<< HEAD
 }
-
 
 function openNav2() {
     document.getElementById("myNav2").style.width = "100%";
@@ -16,7 +14,6 @@ function openNav2() {
 function closeNav2() {
     document.getElementById("myNav2").style.width = "0%";
 }
-
 function openNav3() {
     document.getElementById("myNav3").style.width = "100%";
 }
@@ -24,10 +21,10 @@ function openNav3() {
 function closeNav3() {
     document.getElementById("myNav3").style.width = "0%";
 }
-
-
-
-
-=======
+function openNav4() {
+    document.getElementById("myNav4").style.width = "100%";
 }
->>>>>>> 84f44461cb0ad7289dfcbe7002ef2acb3163361a
+
+function closeNav4() {
+    document.getElementById("myNav4").style.width = "0%";
+}
